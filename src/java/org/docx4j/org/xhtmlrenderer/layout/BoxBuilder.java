1b7b0097cf8e267184d5175739b1c70ac77354e0
Jasper Dijt <jasper.dijt@netive.nl>
2016-11-17 17:06:03 +0100

    HTMLRenderer: bugfix for nullpointer

--- src/java/org/docx4j/org/xhtmlrenderer/layout/BoxBuilder.java
+++ src/java/org/docx4j/org/xhtmlrenderer/layout/BoxBuilder.java
@@ -202,7 +202,7 @@ public class BoxBuilder {
                     child = createInlineBox(text.toString(), parent, parentStyle, textNode);
                     */
 
-                    if (parent.getLocalName().equals("script") 
+                    if (parent != null && parent.getLocalName() != null && parent.getLocalName().equals("script") 
 //                            || textNode.getTextContent().trim().equals("")
                             )  {
                         // This is an important change, for us. It gets rid of a lot of extra unwanted parents inserted
